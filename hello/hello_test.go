af99eb669057f5cf4c27705d682d0f807fb99b62
Fabricio <fabrizzio.cejas.80@gmail.com>
2020-07-11 18:09:52 -0300

    practice 1

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -9,3 +9,10 @@ func TestHello(t *testing.T) {
         t.Errorf("Hello() = %q, expected = %q", result, expected)
     }
 }
+
+func TestProverb(t *testing.T) {
+    want := "Concurrency is not parallelism."
+    if got := Proverb(); got != want {
+        t.Errorf("Proverb() = %q, want %q", got, want)
+    }
+}
